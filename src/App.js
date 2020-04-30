b58fb01462c76e8a397aca818904cac22c4eae7c
dc <chendana95@gmail.com>
2020-04-30 19:50:39 -0400

    updated app.js to ensure that only necessary components are used

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
-import Navbar from "./components/Navbar";
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
+import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Switch>
-        <Route exact path="/"><Home /></Route>
-        <Route exact path="/home"><Home /></Route>
-        <Route exact path="/projects"><Projects /></Route>
-        <Route exact path="/contact"><Contact /></Route>
-        </Switch>
-      </div>
-    </Router>
+    // <Router>
+    //   <div>
+    //     <Navbar />
+    //     <Switch>
+    //     <Route exact path="/"><About /></Route>
+    //     <Route exact path="/home"><About /></Route>
+    //     <Route exact path="/projects"><Projects /></Route>
+    //     <Route exact path="/contact"><Contact /></Route>
+    //     </Switch>
+    //   </div>
+    // </Router>
+        <div>
+         <Navbar />
+         <About />
+         <Projects />
+         <Contact />
+       </div>
   );
 }
 
