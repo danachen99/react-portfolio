626438580503dc5e9dcc301c8f06a439dbb8d60c
dc <chendana95@gmail.com>
2020-05-01 20:10:08 -0400

    updated portfolio and about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,7 +9,14 @@ function About() {
                     <div className="col-lg-8 text-center">
                         <h2 className="text-center mt-0">About Me</h2>
                         <hr className="divider my-4" color="#408eff" />
-                        <p className="text-muted mb-0">Full stack web developer. Penn LPS Coding Boot Camp</p>
+                        <img className="card-img-top" src="../../assets/images/Danapic.jpeg" alt="Dana"/>
+                        <p className="text-muted mb-0"> 
+                                I am a full-stack web developer born and raised in Philadelphia, PA. I enjoy working on my own as well as with others to share ideas in order to grow my knowledge. I am currently reading Fierce Conversations
+                                by Susan Scott and the Four Agreements by Don Miguel Ruiz. These books are guiding me to find my truth and to become a leader through conversations. The Penn LPS Coding Bootcamp has helped me develop and strengthen skills
+                                to search for a career in the tech industry.
+                                <br />
+                                My interests include cooking, baking sweet treats, and going to the gym. My priority is to maintain a healthy lifestyle by doing physical activity as well as have a healthy mindset. I enjoy making cookies and would love to start baking
+                                cakes and pies, especially for the holiday season. I have many gym goals, including achieving my first unassisted push and pull ups! I also want to challenge myself to continue deadlifting and progressing the other compound lifts.</p>
                             <div className="aboutMeButtons">
                                 <a href="./assets" className="btn btn-outline-primary aboutMeButtons" id="viewResume" role="button">Resume</a>
                                 <a href="https://github.com/danachen99" className="btn btn-primary aboutMeButtons" id="githubRepo" role="button"><i className="fab fa-github"></i> Github </a>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,7 +9,7 @@ function Nav() {
                 <div className="col-lg-4 col-sm-6">
                  <div className="card">
                     <div className="card-block">
-                        <img className="card-img-top" src="./assets/images/project1.pnc" alt="IngredienceApp"/>
+                        <img className="card-img-top" src="../../assets/images/project1.png" alt="IngredienceApp"/>
                         <h4 className="card-title">Ingredience Application</h4>
                         <p className="card-text p-y-1">Application that generates recipes from the Spoonacular API using ingrediences that the user inputs, including dietary restrictions.</p>
                         <div className="twoButtons">
@@ -18,7 +18,7 @@ function Nav() {
                           <a href="https://github.com/danachen99/ingredience-app" className="btn btn-primary btn-sm" id="githubRepo"
                             role="button"><i className="fab fa-github"></i> Github Repo</a>
                         </div>
-                        <div className="builtWith">HTML, CSS, Bootstrap, Javascript, jQuery, Spoonacular API</div>
+                        <div className="builtWith">HTML, CSS, Materialize, Javascript, jQuery, Spoonacular API</div>
                       </div>
                     </div>
                   </div>
@@ -26,16 +26,16 @@ function Nav() {
                   <div className="col-lg-4 col-sm-6">
                  <div className="card">
                     <div className="card-block">
-                        <img className="card-img-top" src="./assets/images/project1.pnc" alt="IngredienceApp"/>
-                        <h4 className="card-title">Ingredience Application</h4>
-                        <p className="card-text p-y-1">Application that generates recipes from the Spoonacular API using ingrediences that the user inputs, including dietary restrictions.</p>
+                        <img className="card-img-top" src="../../assets/images/project2.png" alt="IngredienceApp"/>
+                        <h4 className="card-title">Philly Auto Emporium</h4>
+                        <p className="card-text p-y-1">Full-stack web application following the MVC design pattern that utilizes a third-party API (CarMD) to collect car data, AJAX calls to get/post/delete data, Node for back-end, Express for routing, NodeMailer for sending emails, and MySQL for saving/deleting car data for inventory purposes. </p>
                         <div className="twoButtons">
-                          <a href="https://philly-auto-emporium.herokuapp.com/" className="btn btn-outline-primary btn-sm"
+                          <a href="https://philly-car-emporium.herokuapp.com/" className="btn btn-outline-primary btn-sm"
                             id="viewPage" role="button">View Page</a>
                           <a href="https://github.com/danachen99/philly-car-emporium" className="btn btn-primary btn-sm" id="githubRepo"
                             role="button"><i className="fab fa-github"></i> Github Repo</a>
                         </div>
-                        <div className="builtWith">HTML, CSS, Foundation, Javascript, jQuery</div>
+                        <div className="builtWith">HTML, CSS, Foundation, Javascript, jQuery, MySQL, Express</div>
                       </div>
                     </div>
                   </div>
@@ -43,7 +43,24 @@ function Nav() {
                   <div className="col-lg-4 col-sm-6">
                     <div className="card">
                       <div className="card-block">
-                        <img className="card-img-top" src="./assets/img/portfolio/thumbnails/project2.png" alt="Weather Dashboard"/>
+                        <img className="card-img-top" src="/" alt="Weather Dashboard"/>
+                        <h4 className="card-title">Google Books Search</h4>
+                        <p className="card-text p-y-1">React application that allows the user to search for books from an API to save to the database.</p>
+                        <div className="twoButtons">
+                          <a href="https://react-google-books-dc.herokuapp.com/" className="btn btn-outline-primary btn-sm" id="viewPage"
+                            role="button">View Page</a>
+                          <a href="https://github.com/danachen99/google-books-search" className="btn btn-primary btn-sm"
+                            id="githubRepo" role="button"><i className="fab fa-github"></i> Github Repo</a>
+                        </div>
+                        <div className="builtWith">Node.js, Javascript, Mongoose, React.js</div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="col-lg-4 col-sm-6">
+                    <div className="card">
+                      <div className="card-block">
+                        <img className="card-img-top" src="/" alt="Project 2"/>
                         <h4 className="card-title">CLI Employee Directory</h4>
                         <p className="card-text p-y-1">Command line application that will allow the head of a software engineering team to add/remove users as well as view them </p>
                         <div className="twoButtons">
@@ -60,7 +77,7 @@ function Nav() {
                   <div className="col-lg-4 col-sm-6">
                     <div className="card">
                       <div className="card-block">
-                        <img className="card-img-top" src="" alt="Weather Dashboard"/>
+                        <img className="card-img-top" src="../../assets/images/pass-gen.png" alt="Password generator"/>
                         <h4 className="card-title">Password Generator</h4>
                         <p className="card-text p-y-1">Given the length that the user selects, the application will generate a random password.</p>
                         <div className="twoButtons">
@@ -77,7 +94,7 @@ function Nav() {
                   <div className="col-lg-4 col-sm-6">
                     <div className="card">
                       <div className="card-block">
-                        <img className="card-img-top" src="./assets/img/portfolio/thumbnails/project5.png" alt="Weather Dashboard"/>
+                        <img className="card-img-top" src="/" alt="Weather Dashboard"/>
                         <h4 className="card-title">Burger Time</h4>
                         <p className="card-text p-y-1">A burger logger that lets users input the names of burgers they'd like to eat and also devour them.</p>
                         <div className="twoButtons">
@@ -95,7 +112,7 @@ function Nav() {
                    <div className="col-lg-4 col-sm-6">
                     <div className="card">
                       <div className="card-block">
-                        <img className="card-img-top" src="./assets/img/portfolio/thumbnails/project8.png" alt="Weather Dashboard"/>
+                        <img className="card-img-top" src="" alt="Weather Dashboard"/>
                         <h4 className="card-title">Note Taker</h4>
                         <p className="card-text p-y-1">An application that can be used to write, save, and delete notes from the user. This app uses an express backend and saves and retrieves data from a JSON file.</p>
                         <div className="twoButtons">
